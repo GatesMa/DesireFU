b65c36775a5850934302463894d4a3c38cae527a
gatesma <gatesma@foxmail.com>
2021-02-24 11:22:39 +0800

    一些修改

--- dfu-service/src/main/java/cn/gatesma/desirefu/config/db/JooqConfiguration.java
+++ dfu-service/src/main/java/cn/gatesma/desirefu/config/db/JooqConfiguration.java
@@ -1,6 +1,7 @@
 package cn.gatesma.desirefu.config.db;
 
 import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariPoolMXBean;
 import org.jooq.ConnectionProvider;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
@@ -9,6 +10,8 @@ import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
 import org.jooq.impl.DefaultExecuteListenerProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jooq.SpringTransactionProvider;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,8 +19,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.management.JMX;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 import javax.sql.DataSource;
+import java.lang.management.ManagementFactory;
 
 /**
  * User: gatesma
@@ -26,6 +35,10 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class JooqConfiguration {
+
+//    private static HikariPoolMXBean proxy;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JooqConfiguration.class);
 
     @Bean
     @Scope(scopeName = "singleton")
@@ -41,6 +54,38 @@ public class JooqConfiguration {
         context.settings().setRenderNameStyle(RenderNameStyle.AS_IS);
         return context;
     }
+
+//    @Scheduled(cron = "0/1 * * * * *")
+//    public void monitor() {
+//        try {
+//            if (null == proxy) {
+//                MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+//                ObjectName poolName = new ObjectName("com.zaxxer.hikari:type=Pool (hikariCP_dfu)");
+//                proxy = JMX.newMXBeanProxy(mBeanServer, poolName, HikariPoolMXBean.class);
+//            } else {
+//                LOGGER.info("{}, {}, {}, {}", proxy.getTotalConnections(), proxy.getActiveConnections(), proxy.getIdleConnections(), proxy.getThreadsAwaitingConnection());
+//            }
+//        } catch (Throwable cause) {
+//            LOGGER.error("", cause);
+//        }
+//    }
+
+//    @Scheduled(fixedRate = 1000)
+//    public void HikariMonitor() {
+//        HikariDataSource hikariDataSource = (HikariDataSource) dataSource();
+//        hikariDataSource.getHikariPoolMXBean();
+//        if(poolProxy == null) {
+//            LOGGER.info("Hikari not initialized,please wait...");
+//        }else {
+//            LOGGER.info("HikariPoolState = "
+//                    + "Active=[" + String.valueOf(poolProxy.getActiveConnections() + "] "
+//                    + "Idle=[" + String.valueOf(poolProxy.getIdleConnections() + "] "
+//                    + "Wait=["+poolProxy.getThreadsAwaitingConnection()+"] "
+//                    + "Total=["+poolProxy.getTotalConnections()+"]")));
+//        }
+//
+//    }
+
 
     @Bean
     public ConnectionProvider connectionProvider() {
