180627fe65c362c04e12f04c938d9746f90809d4
gatesma <gatesma@foxmail.com>
2021-03-31 14:50:08 +0800

    链接elasticsearch

--- dfu-service/src/main/java/cn/gatesma/desirefu/controller/TestController.java
+++ dfu-service/src/main/java/cn/gatesma/desirefu/controller/TestController.java
@@ -1,10 +1,32 @@
 package cn.gatesma.desirefu.controller;
 
+import cn.gatesma.desirefu.constant.EsConst;
+import cn.gatesma.desirefu.service.EsService;
+import org.elasticsearch.action.search.SearchResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
 /**
  * @author gatesma
  * @date 2020/11/1
  * @desc
  */
+@Controller
 public class TestController {
+
+    @Resource
+    private EsService esService;
+
+    @RequestMapping("/test_es")
+    @ResponseBody
+    public void testEs() {
+
+        SearchResponse searchResponse = esService.queryFromES(EsConst.DESIREFU_SERVICE_INDEX, EsConst.INDEX_TYPE_NORMAL_ACCOUNT, null, null, null, 0, 10);
+        System.out.println(searchResponse);
+    }
+
 
 }
